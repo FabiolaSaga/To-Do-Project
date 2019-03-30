b614ce92ca4dbbd54ea0938cb3afa7a4382b884a
FabiolaSaga <saga.fabiola@gmail.com>
2019-03-30 12:11:40 -0500

    Added functionality for datePicker to expand and collapse

--- ToDoProject/ToDoViewController.swift
+++ ToDoProject/ToDoViewController.swift
@@ -18,6 +18,8 @@ class ToDoViewController: UITableViewController {
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
+    var isEndDatePickerHidden = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         dueDatePickerView.date = Date().addingTimeInterval(24*60*60)
@@ -48,6 +50,37 @@ class ToDoViewController: UITableViewController {
     
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
         updateDueDateLabel(date: dueDatePickerView.date)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        let normalCellHeight = CGFloat(44)
+        let largeCellHeight = CGFloat(200)
+        
+        switch(indexPath) {
+        case [1,0]:
+            return isEndDatePickerHidden ? normalCellHeight : largeCellHeight
+            
+        case [2,0]:
+            return largeCellHeight
+            
+        default: return normalCellHeight
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        switch (indexPath) {
+        case [1,0]:
+            isEndDatePickerHidden = !isEndDatePickerHidden
+            
+            dueDateLabel.textColor = isEndDatePickerHidden ? .black : tableView.tintColor
+            
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+        default: break
+        }
     }
     
     
