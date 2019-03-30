9c1aac834ba3fa193ce21919701cce096abe849d
FabiolaSaga <saga.fabiola@gmail.com>
2019-03-30 11:34:00 -0500

    Added TableViewController Added ToDo Struct Added dynamic Edit button added Button & TextField

---
+++ ToDoProject/ToDo.swift
@@ -0,0 +1,29 @@
+//
+//  ToDo.swift
+//  ToDoProject
+//
+//  Created by Fabiola Saga on 3/30/19.
+//  Copyright © 2019 Fabiola Saga. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo {
+    
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        let todo1 = ToDo(title: "ToDo One", isComplete: false, dueDate: Date(), notes: "Notes1")
+        let todo2 = ToDo(title: "ToDo Two", isComplete: false, dueDate: Date(), notes: "Notes2")
+        let todo3 = ToDo(title: "ToDo Three", isComplete: false, dueDate: Date(), notes: "Notes3")
+        
+        return [todo1, todo2, todo3]
+    }
+}
---
+++ ToDoProject/ToDoTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoProject
+//
+//  Created by Fabiola Saga on 3/30/19.
+//  Copyright © 2019 Fabiola Saga. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var todos = [ToDo]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let savedToDos = ToDo.loadToDos() {
+            todos = savedToDos
+        } else {
+            todos = ToDo.loadSampleToDos()
+        }
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier") else {
+            fatalError("Could not dequeue a cell")
+        }
+        
+        let todo = todos[indexPath.row]
+        cell.textLabel?.text = todo.title
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            todos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    @IBAction func unwindToToDoList(segue: UIStoryboardSegue) {
+        
+    }
+
+}
